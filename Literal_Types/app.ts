44cd463e7f044bcbabb4e726bc27d647c747d84d
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-14 18:37:03 -0500

    Union & Literal Types

---
+++ Literal_Types/app.ts
@@ -0,0 +1,19 @@
+function combine(
+  input1: number | string,
+  input2: number | string,
+  expectedOutput: string
+) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + " " + input2.toString();
+  }
+  return result;
+}
+const combinedAges = combine(30, 6, "as-number");
+console.log(combinedAges);
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+const combinedNames = combine("Max", "Anna", "asText");
+console.log(combinedNames);
