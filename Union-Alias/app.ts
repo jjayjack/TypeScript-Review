ee6b6fe0284d2f694afe2ff9d893bd9d60931191
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-15 07:46:16 -0500

    Void vs Undefined

---
+++ Union-Alias/app.ts
@@ -0,0 +1,12 @@
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+function printResult(num: number) {
+  console.log("Result: " + num);
+}
+printResult(add(5, 12));
+console.log(printResult(add(5, 12)));
+/*Void - standard to be used when constructing function that does not return a value - can be assigned or inferred
+Undefined can be used as a datatype but 
+*/
